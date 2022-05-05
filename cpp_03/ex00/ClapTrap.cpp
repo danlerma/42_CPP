77873b6a7ef5065d20c38a78db95f7396b974067
Dan <daniellerma05218@gmail.com>
2022-05-05 17:37:41 +0200

    ex03

--- cpp_03/ex00/ClapTrap.cpp
+++ cpp_03/ex00/ClapTrap.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlerma-c <dlerma-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/05 15:54:11 by dlerma-c          #+#    #+#             */
+/*   Updated: 2022/05/05 16:02:16 by dlerma-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include"ClapTrap.hpp"
 
 //constructor
@@ -43,25 +55,30 @@ ClapTrap&	ClapTrap::operator=(const ClapTrap& obj)
 }
 
 //functions
-	void ClapTrap::attack(std::string const & target)
-	{
+void ClapTrap::attack(std::string const & target)
+{
+	if (this->hitpoints > 0)
 		cout << "ClapTrap" << this->name << " attack" << target << ", causing " << this->attack_damage << " points of damage!" << endl;
-	}
+	else
+		cout << this->name << "is dead. Can't do anything." << endl;
+}
 
-	void ClapTrap::takeDamage(unsigned int amount)
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if ((int)amount >= this->energy_points)
 	{
-		if ((int)amount >= this->energy_points)
-		{
-			cout << this->name << "is dead. Can't do anything." << endl;
-			this->energy_points -= amount;
-			return ;
-		}
+		cout << this->name << "is dead. Can't do anything." << endl;
 		this->energy_points -= amount;
-		cout << "Now " << this->name << "has " << this->energy_points  << " of energy points."<< endl;
+		return ;
 	}
+	this->energy_points -= amount;
+	cout << "Now " << this->name << "has " << this->energy_points  << " of energy points."<< endl;
+}
 
-	void ClapTrap::beRepaired(unsigned int amount)
-	{
-		this->energy_points += amount;
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->hitpoints <= 0)
 		cout << "Now " << this->name << "has " << this->energy_points  << " of energy points."<< endl;
-	}
+	this->energy_points += amount;
+	cout << "Now " << this->name << "has " << this->energy_points  << " of energy points."<< endl;
+}
